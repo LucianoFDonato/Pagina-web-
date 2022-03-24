979ac935e741ebeb87d244bb6ae9156be002fab1
Luciano <lucianofdonato@gmail.com>
2022-03-24 12:33:06 -0300

    24-03-2022 12:33

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const open = document.getElementById('open');
 const modal_container = document.getElementById('modal-container');
 const close = document.getElementById('close');
 
-open.addEventListener('click', () =>{
+open.addEventListener('click', () => {
     modal_container.classList.add('show');
 });
 
-close.addEventListener('click', () =>{
+close.addEventListener('click', () => {
     modal_container.classList.remove('show');
 });
 
@@ -16,11 +16,11 @@ const open_2 = document.getElementById('open_2');
 const modal_container_2 = document.getElementById('modal-container-2');
 const close_2 = document.getElementById('close_2');
 
-open_2.addEventListener('click', () =>{
+open_2.addEventListener('click', () => {
     modal_container_2.classList.add('show');
 });
 
-close_2.addEventListener('click', () =>{
+close_2.addEventListener('click', () => {
     modal_container_2.classList.remove('show');
 });
 
@@ -29,11 +29,11 @@ const open_3 = document.getElementById('open_3');
 const modal_container_3 = document.getElementById('modal-container-3');
 const close_3 = document.getElementById('close_3');
 
-open_3.addEventListener('click', () =>{
+open_3.addEventListener('click', () => {
     modal_container_3.classList.add('show');
 });
 
-close_3.addEventListener('click', () =>{
+close_3.addEventListener('click', () => {
     modal_container_3.classList.remove('show');
 });
 
@@ -49,61 +49,17 @@ Toggle.addEventListener("click", () => {
 
 /* Transition */
 
-function delay(n) {
-    n = n || 2000;
-    return new Promise((done) => {
-        setTimeout(() => {
-            done();
-        }, n);
-    });
-}
+window.sr = ScrollReveal();
 
-function pageTransition() {
-    var tl = gsap.timeline();
-    tl.to(".loading-screen", {
-        duration: 1.2,
-        width: "100%",
-        left: "0%",
-        ease: "Expo.easeInOut",
-    });
+sr.reveal('.nav', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '-100px',
+});
 
-    tl.to(".loading-screen", {
-        duration: 1,
-        width: "100%",
-        left: "100%",
-        ease: "Expo.easeInOut",
-        delay: 0.3,
-    });
-    tl.set(".loading-screen", { left: "-100%" });
-}
 
-function contentAnimation() {
-    var tl = gsap.timeline();
-    tl.from(".animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2 });
-}
-
-$(function () {
-    barba.init({
-        sync: true,
-
-        transitions: [
-            {
-                async leave(data) {
-                    const done = this.async();
-
-                    pageTransition();
-                    await delay(1000);
-                    done();
-                },
-
-                async enter(data) {
-                    contentAnimation();
-                },
-
-                async once(data) {
-                    contentAnimation();
-                },
-            },
-        ],
-    });
+sr.reveal('.cards', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '-140px',
 });
