efec9e0f1034f06c8815bc97343f89da02e5528c
Luciano <lucianofdonato@gmail.com>
2022-05-10 23:30:59 -0300

    10-05-2022 11:30

--- js/main.js
+++ js/main.js
@@ -51,12 +51,6 @@ document.addEventListener("click", () =>{
     Menu.classList.add("nav-menu");
 })
 
-const Main = document.querySelector(".nav.menu");
-const btnCambiar = document.getElementById("#nav");
-
-btnCambiar.addEventListener("click", () =>{
-    document.Main.left = "100%";
-})
 
 /* Chances bars */
 
